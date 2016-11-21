00d8a983bfdbc046d8891aa4efa9058bdcbaa7ce
David Merrick <david.merrick@jivesoftware.com>
2016-11-21 07:00:25 -0800

    Started writing unit tests

---
+++ src/test/Test.js
@@ -0,0 +1,30 @@
+var expect = require('chai').expect;
+var rewire = require('rewire');
+var SpeechHelper = require('../utils/SpeechHelper');
+var Arrival = rewire('trimet-api-client/Arrival');
+
+describe("Speech Helper Test", function(){
+    it("Should output correctly for a single arrival.", function(){
+        var arrivalData = {
+            departed: null,
+            scheduled: null,
+            shortSign: null,
+            blockPosition: null,
+            estimated: null,
+            dir: null,
+            route: null,
+            detour: null,
+            piece: null,
+            fullSign: "20",
+            block: null,
+            locid: null,
+            status: null
+        };
+
+        var arrival = new Arrival(arrivalData);
+        arrival.__set__("getMinutesUntilArrival", function(){
+            return 5;
+        });
+        expect(arrival.getMinutesUntilArrival()).to.equal(5);
+    });
+});
