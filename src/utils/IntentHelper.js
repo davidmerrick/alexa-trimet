7ef77f076fdb06288b8c86f84f2ae559187796cd
David Merrick <david.merrick@jivesoftware.com>
2016-11-24 08:51:17 -0800

    Added try catch for intent helper

--- src/utils/IntentHelper.js
+++ src/utils/IntentHelper.js
@@ -6,23 +6,31 @@ var TriMetAPIInstance = new TriMetAPI(process.env.TRIMET_API_KEY);
 
 var IntentHelper = module.exports = {
     getSingleNextArrival: function(stopID, busID, callback){
-        TriMetAPIInstance.getNextArrivalForBus(stopID, busID, function(arrival){
-            if(!arrival){
-                callback(`Sorry, I was not able to find information for bus ${busID} at stop ${stopID}`);
-            }
-            var minutesRemaining = arrival.getMinutesUntilArrival();
-            var minutePronunciation = SpeechHelper.getMinutePronunciation(minutesRemaining);
-            callback(`${minutePronunciation} until the next bus ${busID} at stop ${stopID}`);
-        });
+        try {
+            TriMetAPIInstance.getNextArrivalForBus(stopID, busID, function (arrival) {
+                if (!arrival) {
+                    callback(`Sorry, I was not able to find information for bus ${busID} at stop ${stopID}`);
+                }
+                var minutesRemaining = arrival.getMinutesUntilArrival();
+                var minutePronunciation = SpeechHelper.getMinutePronunciation(minutesRemaining);
+                callback(`${minutePronunciation} until the next bus ${busID} at stop ${stopID}`);
+            });
+        } catch(error){
+            callback(`Sorry, I was not able to find information for bus ${busID} at stop ${stopID}`);
+        }
     },
     getAllNextArrivals: function(stopID, callback) {
-        TriMetAPIInstance.getSortedFilteredArrivals(stopID, function (arrivals) {
-            if (!arrivals || arrivals.length === 0) {
-                callback(`Sorry, I was not able to find arrival information for stop ${stopID}`);
-            }
-            var speechOutput = SpeechHelper.buildArrivalsResponse(stopID, arrivals);
-            callback(speechOutput);
-        });
+        try {
+            TriMetAPIInstance.getSortedFilteredArrivals(stopID, function (arrivals) {
+                if (!arrivals || arrivals.length === 0) {
+                    callback(`Sorry, I was not able to find arrival information for stop ${stopID}`);
+                }
+                var speechOutput = SpeechHelper.buildArrivalsResponse(stopID, arrivals);
+                callback(speechOutput);
+            });
+        } catch(error){
+            callback(`Sorry, I was not able to find arrival information for stop ${stopID}`);
+        }
     }
 };
 
